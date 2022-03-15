a02030da7a307460a59757afde3996181793b45b
Nikolai Golub <nikolai.golub@consensys.net>
2022-03-15 12:58:12 +0100

    Implement comparison

--- contracts/Rollup.sol
+++ contracts/Rollup.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
-
 contract Rollup {
 
     bytes32 rootHashBytes;
@@ -10,12 +9,73 @@ contract Rollup {
     bool something;
     // "Raw" section
 
+    event Done(uint256 rootHash, bool result);
 
     function submitBatchRaw(
         bytes32 rootHash,
-        bytes calldata transactions
+        bytes calldata
     ) external {
         rootHashBytes = rootHash;
+        uint256[] memory parsed = parseRawTransactions();
+
+        bool result = processParsed(parsed);
+
+        emit Done(uint256(rootHash), result);
+    }
+
+    function parseRawTransactions() internal pure returns (uint256[] memory parsed) {
+
+        uint offset = 100;
+        bytes memory temp = new bytes(32);
+        copyFromCallData(temp, offset);
+
+        (uint16 transactionsCount, uint256 bytesRead) = readU16(temp, 0);
+        offset += bytesRead;
+
+        parsed = new uint256[](transactionsCount);
+
+        uint16 element;
+        for (uint16 i = 0; i < transactionsCount; i++) {
+            copyFromCallData(temp, offset);
+            (element, bytesRead) = readU16(temp, 0);
+            offset += bytesRead;
+            parsed[i] = uint256(element);
+        }
+
+        return parsed;
+    }
+
+    function copyFromCallData(bytes memory temp, uint256 _offset) internal pure {
+        assembly {
+            calldatacopy(add(temp, 32), _offset, 0x2)
+        }
+    }
+
+    function readU16(bytes memory _bytes, uint256 _offset)
+    internal
+    pure
+    returns (uint16, uint256)
+    {
+        require(_bytes.length >= _offset + 2, "slicing out of range");
+        uint16 x;
+        assembly {
+            x := mload(add(_bytes, add(0x2, _offset)))
+        }
+        return (x, _offset + 2);
+    }
+
+    function processParsed(uint256[] memory transactions) internal pure returns (bool) {
+        uint256 counter = 0;
+        bool result = false;
+        for (uint i = 0; i < transactions.length; i++) {
+            if (transactions[i] % 2 == 0) {
+                counter += 1;
+            }
+        }
+        if (counter % 2 == 0) {
+            result = true;
+        }
+        return result;
     }
 
 
@@ -25,5 +85,22 @@ contract Rollup {
         uint256[] calldata transactions
     ) external {
         rootHashU256 = rootHash;
+        bool result = processCallData(transactions);
+
+        emit Done(rootHash, result);
+    }
+
+    function processCallData(uint256[] calldata transactions) internal pure returns (bool) {
+        uint256 counter = 0;
+        bool result = false;
+        for (uint i = 0; i < transactions.length; i++) {
+            if (transactions[i] % 2 == 0) {
+                counter += 1;
+            }
+        }
+        if (counter % 2 == 0) {
+            result = true;
+        }
+        return result;
     }
 }
--- test/DummyRollup.js
+++ test/DummyRollup.js
@@ -7,7 +7,9 @@ describe("Rollup", function () {
     const rootHash = "0x1c433e3e9626287515e1a1fcf9f8bf66ca6d3270a8b8589f9a7543b1f918353d";
     const transactions = Array.from(Array(1000).keys())
     const transactionsU256 = transactions.map(t => ethers.BigNumber.from(t));
-    const transactionsRaw = "0x" + transactionsU256.map(t => t.toHexString().replace("0x", "")).join('')
+    const transactionsRaw = "0x"
+        + ethers.BigNumber.from(transactions.length).toHexString().replace("0x", "").padStart(4, '0')
+        + transactionsU256.map(t => t.toHexString().replace("0x", "").padStart(4, '0')).join('')
 
     before(async function () {
         const Rollup = await ethers.getContractFactory("Rollup");
